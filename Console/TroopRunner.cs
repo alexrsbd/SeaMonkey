67b9e5daa1422c25c40f5b9544d3a20d0e46820b
Akira Yamamoto <3007213+akirayamamoto@users.noreply.github.com>
2022-01-27 23:40:25 +1100

    Added timings for each monkey run.

--- Console/TroopRunner.cs
+++ Console/TroopRunner.cs
@@ -1,4 +1,5 @@
 using Serilog;
+using SerilogTimings;
 
 namespace SeaMonkey
 {
@@ -10,10 +11,14 @@ namespace SeaMonkey
 
             var commandFactory = new MonkeyCommandFactory(request);
 
+            using var troopOp = Operation.Time("Running this troop");
             foreach (var breed in request.GetMonkeyBreeds())
             {
                 var command = commandFactory.GetCommand(breed);
-                Log.Information($"Running {command.GetType().Name}...");
+
+                Log.Information("Running {Command}...", command.GetType().Name);
+                using var breedOp = Operation.Time("Running {Command}", command.GetType().Name);
+
                 command.Run();
             }
         }
